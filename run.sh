d8ba9b3e20a804ff8c6228715c3aad836651720d
Andy Phu <andy.jiapao.phu@gmail.com>
2025-12-10 18:05:57 +0000

    docs(gum): clarify how to kill gum background

--- run.sh
+++ run.sh
@@ -9,4 +9,8 @@ gum  --user-name "andrew" --model "Qwen/Qwen3-VL-8B-Instruct"
 
 
 # may run on lab server for long inference with nohup ./run.sh &
+# discard logs: nohup ./run.sh 2>&1 | tail -n 1000 > gum.log &
+
+
 # kill with pkill -f "gum --user-name"
+# ps aux | grep -v grep | grep gum
